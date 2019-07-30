76dc532b1930bf5b4e40c74b6b4df1b14db6478e
msalikhova <msalikhova@unomaha.edu>
2019-07-30 15:33:11 -0500

    changed api url to dr. royce url

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import axios from 'axios';
-const API_URL = 'https://efsrest8380.herokuapp.com/'; /* http://groyce.pythonanywhere.com/ or http://localhost:8000 */
+const API_URL = 'http://groyce.pythonanywhere.com/'
 
 export class APIService {
   constructor() {
